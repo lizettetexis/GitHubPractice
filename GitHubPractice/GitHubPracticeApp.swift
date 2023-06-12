5277a903ec45b6064c7b559f5650b56766711939
lizettetexis <lizette.t2024@gmail.com>
2023-06-12 11:22:26 -0400

    Initial Commit

---
+++ GitHubPractice/GitHubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracticeApp.swift
+//  GitHubPractice
+//
+//  Created by scholar on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
