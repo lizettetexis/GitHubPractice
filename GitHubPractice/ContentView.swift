f33873bdd46606ce19818cac2ea6812aafe02d86
lizettetexis <lizette.t2024@gmail.com>
2023-06-14 15:46:51 -0400

    For you

--- GitHubPractice/ContentView.swift
+++ GitHubPractice/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tasks = ""
+    @State private var textTitle2 = ""
     var body: some View {
+        ZStack{
             VStack {
                 Image("nn6")
                     .padding(.bottom, 30.0)
@@ -17,17 +20,19 @@ struct ContentView: View {
                         .font(.title)
                         .foregroundColor(Color(hue: 0.463, saturation: 0.268, brightness: 0.918))
                         .multilineTextAlignment(.center))
-                    
-       //         Button("Your Tasks"){
-        //            print("jwdjjkdnjkw")
-                }
-             //   .font(.title)
-           //     .buttonStyle.borderedProminent)
-            //        .tint(Color(hue: 0.914, saturation: 0.489, brightness: 1.0))
-                }
                 
+                
+                //         Button("Your Tasks"){
+                //            print("jwdjjkdnjkw")
+                //   }
+                //   .font(.title)
+                //     .buttonStyle.borderedProminent)
+                //        .tint(Color(hue: 0.914, saturation: 0.489, brightness: 1.0))
+                //     }
             }
-       
+        }
+        }
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
