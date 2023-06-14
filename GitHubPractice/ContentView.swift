e3983df786b711f047a516976ca2a60d9220fb05
lizettetexis <lizette.t2024@gmail.com>
2023-06-14 15:28:47 -0400

    Image Set and Welcome Text

--- GitHubPractice/ContentView.swift
+++ GitHubPractice/ContentView.swift
@@ -13,12 +13,21 @@ struct ContentView: View {
                 Image("nn6")
                     .padding(.bottom, 30.0)
                     .ignoresSafeArea()
-              
+                    .overlay(Text("Welcome back, user!")
+                        .font(.title)
+                        .foregroundColor(Color(hue: 0.463, saturation: 0.268, brightness: 0.918))
+                        .multilineTextAlignment(.center))
+                    
+       //         Button("Your Tasks"){
+        //            print("jwdjjkdnjkw")
+                }
+             //   .font(.title)
+           //     .buttonStyle.borderedProminent)
+            //        .tint(Color(hue: 0.914, saturation: 0.489, brightness: 1.0))
+                }
                 
             }
-            .padding()
-    }
-}
+       
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
