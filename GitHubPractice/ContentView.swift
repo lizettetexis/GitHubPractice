24c46b8d5f9791eba843b93d28916dfa4cd2815a
Angela <a>
2023-06-14 22:16:15 -0500

    Home page finished, but buttons don't work yet, only stylized

--- GitHubPractice/ContentView.swift
+++ GitHubPractice/ContentView.swift
@@ -12,30 +12,45 @@ struct ContentView: View {
     @State private var textTitle2 = ""
     var body: some View {
         ZStack{
-            VStack {
-                Image("nn6")
-                    .padding(.bottom, 30.0)
-                    .ignoresSafeArea()
-                    .overlay(Text("Welcome back, user!")
-                        .font(.title)
-                        .foregroundColor(Color(hue: 0.463, saturation: 0.268, brightness: 0.918))
-                        .multilineTextAlignment(.center))
-                
-                
-                //         Button("Your Tasks"){
-                //            print("jwdjjkdnjkw")
-                //   }
-                //   .font(.title)
-                //     .buttonStyle.borderedProminent)
-                //        .tint(Color(hue: 0.914, saturation: 0.489, brightness: 1.0))
-                //     }
+            Image("nn6")
+                .padding(.bottom, 30.0)
+                .ignoresSafeArea()
+                .overlay(Text("Welcome back, user!")
+                    .font(.title)
+                    .foregroundColor(Color(hue: 0.463, saturation: 0.268, brightness: 0.918))
+                    .multilineTextAlignment(.center))
+            
+            VStack{
+                Button {
+                } label: {
+                    Text("Your Tasks")
+                        .foregroundColor(.teal)
+                        .font(.system(size: 33))
+                        .padding(.all, 90.0)
+                        .frame(height: 80.0)
+                        .background(Color(hue: 0.924, saturation: 0.762, brightness: 0.767))
+                        .cornerRadius(8)
+                }
+                .padding(.top, -50.0)
+                Button {
+                    } label: {
+                        Text("Your Tasks")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 33))
+                            .padding(.all, 90.0)
+                            .frame(height: 80.0)
+                            .background(Color(hue: 0.924, saturation: 0.762, brightness: 0.767))
+                            .cornerRadius(8)
+                    }
+                .padding(.top, 10.0)
             }
-        }
+            .position(x: 241, y:600)
         }
     }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
