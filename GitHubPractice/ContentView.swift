0f19c2793cb9d9e699bd76fd9743367422d9edfd
Angela <a>
2023-06-16 10:19:20 -0500

    fixed buttons

--- GitHubPractice/ContentView.swift
+++ GitHubPractice/ContentView.swift
@@ -23,51 +23,38 @@ struct ContentView: View {
                 
                 VStack(alignment: .center){
                     Button {
-                    } label: { NavigationLink(destination: SecondView()){
-                        Text("Your Tasks")
-                            .foregroundColor(Color(red: 0.706, green: 0.932, blue: 0.719))
-                            .font(.system(size: 33))
-                         //   .padding(.trailing, 180.0)
-                         //   .padding(.leading, 120.0)
-                            .frame(width: 300.0, height: 100.0)
-                            .background(Color(red: 0.037, green: 0.175, blue: 0.469))
-                            .cornerRadius(4)
-                         //   .overlay(Text("Your Tasks"))
-                         //   .font(.title)
-                         //   .tint(Color(red: 0.706, green: 0.932, blue: 0.719))
-                    }
-                   // .padding(.top, -10.0)
-                    //.padding(.bottom, 800.0 )
-                   // .padding(.leading, 1075.0)
-                        Button {
-                        } label: {
-                            NavigationLink(destination: YourPlant()){
-                                Text("Your Plant ")
-                                    .foregroundColor(Color(red: 0.706, green: 0.932, blue: 0.719))
-                                    .font(.system(size: 33))
-                                    .multilineTextAlignment(.center)
-                                  //  .padding([.top, .bottom, .trailing], 535.0)
-                                  //  .padding(.leading, .0)
-                                   // .padding(.leading, 500.0)
-                                    .frame(width: 300.0, height: 100.0)
-                                    .background(Color(red: 0.037, green: 0.175, blue: 0.469))
-                                    .cornerRadius(4)
-                                 //   .overlay(Text("Your Plant"))
-                                 //   .font(.title)
-                                 //   .tint(Color(red: 0.706, green: 0.932, blue: 0.719))
-                                    .padding(.top, 250)
-                                   
-                            }
+                    } label: {
+                        NavigationLink(destination: SecondView()){
+                            Text("Your Tasks")
+                                .foregroundColor(Color(red: 0.706, green: 0.932, blue: 0.719))
+                                .font(.system(size: 33))
+                                .frame(width: 300.0, height: 100.0)
+                                .background(Color(red: 0.037, green: 0.175, blue: 0.469))
+                                .cornerRadius(4)
                         }
-                  //     .padding(.top, 245.0)
-                 //    .padding(.trailing, 1000)
                     }
-                    .position(x:400, y:500)
+                    
+                    Button {
+                    } label: {
+                        NavigationLink(destination: YourPlant()){
+                            Text("Your Plant ")
+                                .foregroundColor(Color(red: 0.706, green: 0.932, blue: 0.719))
+                                .font(.system(size: 33))
+                                .multilineTextAlignment(.center)
+                                .frame(width: 300.0, height: 100.0)
+                                .background(Color(red: 0.037, green: 0.175, blue: 0.469))
+                                .cornerRadius(4)
+                        }
+                    }
+                    .padding(.top, 15.0)
                 }
+                .position(x:240, y:560)
             }
         }
     }
 }
+                        
+                        
         struct ContentView_Previews: PreviewProvider {
             static var previews: some View {
                 ContentView()
